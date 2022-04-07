1cf581a35b94d7aa5cc32604ea46bc9e8f6df695
AlnaLouis <alnacl2001@gmail.com>
2022-04-07 22:11:32 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+//  {path:'menu',component:MenuComponent},
+  {path:'cart',component:CartComponent},
+  //{path:'login',component:LoginComponent},
+ // {path:'register',component:RegisterComponent},
+ // {path:'about us',component:AboutusComponent},
+ // {path:'contact us',component:ContactusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
